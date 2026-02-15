f3a72301d10ff6f84b6b0944bc61c32d7ef7eba1
Eleonora <devops.nora@gmail.com>
2026-02-15 15:58:33 -0500

    add new info

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,22 +1,23 @@
 variable "aws_region" {
-  description = "Регион, где разворачиваем MinIO"
+  description = "Регион AWS для развертывания"
   type        = string
-  default     = "eu-central-1"
+  default     = "us-east-1"  # Установи здесь тот регион, где твой ключ
 }
 
 variable "instance_type" {
-  description = "Тип сервера"
+  description = "Тип инстанса EC2"
   type        = string
   default     = "t3.medium"
 }
 
 variable "key_name" {
-  description = "Имя твоего SSH ключа в панели AWS"
+  description = "Имя SSH ключа, созданного в AWS"
   type        = string
+  # Значение по умолчанию не ставим, чтобы случайно не запушить его в GitHub
 }
 
 variable "project_name" {
   description = "Название проекта для тегов"
   type        = string
-  default     = "minio-prod"
+  default     = "minio-production"
 }
