701cc92941b496fcf0c17edb59fac72a69acd15f
Eleonora <devops.nora@gmail.com>
2026-02-17 17:28:35 -0500

    adeed aviable zone "us-east-1a

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,9 +7,13 @@ variable "aws_region" {
 variable "instance_type" {
   description = "Тип инстанса EC2"
   type        = string
-  default     = "t3.medium"
+  default     = "t3.micro"
 }
-
+variable "availability_zone" {
+  description = "Зона доступности для инстанса"
+  type        = string
+  default     = "us-east-1a"  # Установи здесь ту же зону, что и у твоего ключа
+}
 variable "key_name" {
   description = "Имя SSH ключа, созданного в AWS"
   type        = string
